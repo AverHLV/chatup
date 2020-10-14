45f0c7aad8c7890690753823e6066f24663fb778
Aver <moc9.cs4ever@gmail.com>
2020-10-14 20:45:44 +0300

    'Create' command updated

--- chatup/apps/chat/management/commands/create.py
+++ chatup/apps/chat/management/commands/create.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
-from random import choice
+from random import choice, randint
 
 from ... import models
 from ..utils import debug_required
@@ -11,18 +11,31 @@ class Command(BaseCommand):
     help = 'Helper for database populating'
     requires_migrations_checks = True
 
-    brod_number_default: int = 10
+    dests = ['broadcasts', 'messages']
+
+    broadcast_number_default: int = 10
+    message_number_default: int = 20
 
     def add_arguments(self, parser) -> None:
         parser.add_argument(
             '-bn',
             '--brod_num',
             type=int,
-            help=f'Broadcasts number to create, {self.brod_number_default} by default'
+            default=0,
+            dest=self.dests[0],
+            help=f'Broadcasts number to create, {self.broadcast_number_default} by default'
         )
 
-    @debug_required
-    def handle(self, *args, **options) -> None:
+        parser.add_argument(
+            '-mn',
+            '--msg_num',
+            type=int,
+            default=0,
+            dest=self.dests[1],
+            help=f'Messages number to create, {self.message_number_default} by default'
+        )
+
+    def create_broadcasts(self, count: int) -> None:
         streamers = models.CustomUser.objects.filter(role__sid=models.STREAMER_ROLE_SID)
 
         if not len(streamers):
@@ -32,11 +45,48 @@ class Command(BaseCommand):
             models.Broadcast(
                 title=f'Stream #{i}',
                 source_link=f'https://streams.com/stream{i}',
-                streamer=choice(streamers)
+                streamer=choice(streamers),
+                is_active=choice([True, False]),
+                watchers_count=randint(0, 500)
             )
 
-            for i in range(options.get('--brod_num', self.brod_number_default))
+            for i in range(count)
         ]
 
         models.Broadcast.objects.bulk_create(broadcasts)
         self.stdout.write(self.style.SUCCESS(f'{len(broadcasts)} broadcasts created successfully'))
+
+    def create_messages(self, count: int) -> None:
+        broadcasts = models.Broadcast.objects.all()
+        users = models.CustomUser.objects.all()
+        moderators = models.CustomUser.objects.filter(role__sid=models.MODER_ROLE_SID)
+
+        if not len(broadcasts) or not len(users) or not len(moderators):
+            raise CommandError('No needed data in database')
+
+        messages = [
+            models.Message(
+                text='Message text',
+                broadcast=choice(broadcasts),
+                author=choice(users),
+                deleter=choice(moderators) if i % 5 else None
+            )
+
+            for i in range(count)
+        ]
+
+        models.Message.objects.bulk_create(messages)
+        self.stdout.write(self.style.SUCCESS(f'{len(messages)} messages created successfully'))
+
+    @debug_required
+    def handle(self, *args, **options) -> None:
+        if not any(options[destination] for destination in self.dests):
+            self.create_broadcasts(self.broadcast_number_default)
+            self.create_messages(self.message_number_default)
+
+        else:
+            if options[self.dests[0]]:
+                self.create_broadcasts(options[self.dests[0]])
+
+            if options[self.dests[1]]:
+                self.create_messages(options[self.dests[1]])
